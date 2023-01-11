4b993ff07319fae86b2a28de55a9676805baf76a
npty <nptytn2@gmail.com>
2023-01-11 13:44:33 +0700

    feat: Add MsgTransfer payload builder

--- src/utils/payloadBuilder.ts
+++ src/utils/payloadBuilder.ts
@@ -3,6 +3,8 @@ import {
   protobufPackage as EvmProtobufPackage,
 } from "@axelar-network/axelarjs-types/axelar/evm/v1beta1/tx";
 // import {} from "@axelar-network/axelarjs-types/axelar/evm/v1beta1/event";
+import { MsgTransfer } from "cosmjs-types/ibc/applications/transfer/v1/tx";
+import { Height } from "cosmjs-types/ibc/core/client/v1/client";
 import {
   ExecuteGeneralMessageWithTokenRequest,
   protobufPackage as AxelarProtobufPackage,
@@ -10,6 +12,8 @@ import {
 import { toAccAddress } from "@cosmjs/stargate/build/queryclient/utils";
 import { fromHex } from "@cosmjs/encoding";
 import { utils } from "ethers";
+import { Coin, SigningStargateClient } from "@cosmjs/stargate";
+import Long from "long";
 
 /**
  * Get payload for confirm gateway tx on evm chain
@@ -54,3 +58,42 @@ export function getExecuteGeneralMessageWithTokenPayload(
     },
   ];
 }
+
+export function getMsgIBCTransfer(
+  senderAddress: string,
+  recipientAddress: string,
+  sourceChannel: string,
+  amount: string
+) {
+    const sourcePort = "transfer";
+    const timeoutHeight: Height = {
+      revisionHeight: Long.fromNumber(100),
+      revisionNumber: Long.fromNumber(100)
+    };
+    const sendToken: Coin = {
+      amount,
+      denom: 'uusda'
+    };
+  return {
+    typeUrl: "/ibc.applications.transfer.v1.MsgTransfer",
+    value: MsgTransfer.fromPartial({
+      sourcePort: sourcePort,
+      sourceChannel: sourceChannel,
+      sender: senderAddress,
+      receiver: recipientAddress,
+      token: sendToken,
+      timeoutHeight
+    })
+  };
+}
+
+// function get(
+//   senderAddress: string,
+//   signer: SigningStargateClient
+// ) {
+//     const fee = {
+//       gas: "100000",
+//       amount: [{ denom: "uvx", amount: "30000" }]
+//     }
+//     return signer.sign(senderAddress, [transferMsg], fee, "");
+//   }
