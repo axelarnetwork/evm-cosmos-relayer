ced4eaf2d1ec09839adf9eb04ca5957f1eaf319b
npty <nptytn2@gmail.com>
2023-01-17 12:53:01 +0700

    chore: update env handling

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,34 +1,45 @@
-import "dotenv/config";
-import { CosmosNetworkConfig, EvmNetworkConfig } from "./types";
+import 'dotenv/config';
+import { CosmosNetworkConfig, EvmNetworkConfig } from './types';
+
+export const env = {
+  DEV: process.env.DEV || false,
+  AXELAR_MNEMONIC: process.env.AXELAR_MNEMONIC || '',
+  EVM_PRIVATE_KEY: process.env.EVM_PRIVATE_KEY || '',
+  HERMES_METRIC_URL:
+    process.env.HERMES_METRIC_URL || 'http://localhost:3001/metrics',
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    'postgresql://postgres:postgres@localhost:5432/relayer',
+};
 
 const demo: CosmosNetworkConfig = {
-  mnemonic: process.env.AXELAR_MNEMONIC || "",
-  chainId: "demo-chain",
-  denom: "udemo",
+  mnemonic: env.AXELAR_MNEMONIC,
+  chainId: 'demo-chain',
+  denom: 'udemo',
   rpcUrl:
-    "http://a1b287720a05545eb8e2f6c769a1af6b-1437958231.us-east-2.elb.amazonaws.com:26657",
+    'http://a1b287720a05545eb8e2f6c769a1af6b-1437958231.us-east-2.elb.amazonaws.com:26657',
   lcdUrl:
-    "http://a1b287720a05545eb8e2f6c769a1af6b-1437958231.us-east-2.elb.amazonaws.com:1317",
-  ws: "ws://a1b287720a05545eb8e2f6c769a1af6b-1437958231.us-east-2.elb.amazonaws.com:26657/websocket"
+    'http://a1b287720a05545eb8e2f6c769a1af6b-1437958231.us-east-2.elb.amazonaws.com:1317',
+  ws: 'ws://a1b287720a05545eb8e2f6c769a1af6b-1437958231.us-east-2.elb.amazonaws.com:26657/websocket',
 };
 
 const devnet: CosmosNetworkConfig = {
-  mnemonic: process.env.AXELAR_MNEMONIC || "",
-  chainId: "devnet-vx",
-  denom: "uvx",
+  mnemonic: env.AXELAR_MNEMONIC,
+  chainId: 'devnet-vx',
+  denom: 'uvx',
   rpcUrl:
-    "http://a84bc226b379f4142928245039a11d4a-1282067752.us-east-2.elb.amazonaws.com:26657",
+    'http://a84bc226b379f4142928245039a11d4a-1282067752.us-east-2.elb.amazonaws.com:26657',
   lcdUrl:
-    "http://a84bc226b379f4142928245039a11d4a-1282067752.us-east-2.elb.amazonaws.com:1317",
-  ws: "ws://a84bc226b379f4142928245039a11d4a-1282067752.us-east-2.elb.amazonaws.com:26657/websocket"
+    'http://a84bc226b379f4142928245039a11d4a-1282067752.us-east-2.elb.amazonaws.com:1317',
+  ws: 'ws://a84bc226b379f4142928245039a11d4a-1282067752.us-east-2.elb.amazonaws.com:26657/websocket',
 };
 
 const ganache0: EvmNetworkConfig = {
-  name: "ganache-0",
-  privateKey: process.env.EVM_PRIVATE_KEY || "",
+  name: 'ganache-0',
+  privateKey: env.EVM_PRIVATE_KEY,
   rpcUrl:
-    "http://a087b4719fc8944a0952490cf1020812-853925870.us-east-2.elb.amazonaws.com:7545",
-  gateway: "0xE720c5C38028Ca08DA47E179162Eca2DD255B6eC",
+    'http://a087b4719fc8944a0952490cf1020812-853925870.us-east-2.elb.amazonaws.com:7545',
+  gateway: '0xE720c5C38028Ca08DA47E179162Eca2DD255B6eC',
 };
 
 export const config = {
@@ -37,6 +48,6 @@ export const config = {
     devnet,
   },
   evm: {
-    "ganache-0": ganache0,
+    'ganache-0': ganache0,
   },
 };
