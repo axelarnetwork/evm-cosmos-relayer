d3088ea88a45a955694851f9411fdc85dc1fc6ac
npty <nptytn2@gmail.com>
2023-01-18 20:47:34 +0700

    chore: formatting hardhat.config.ts

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,14 +1,13 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-// import "@nomicfoundation/hardhat-toolbox";
-import { evm } from "./constant";
-import "./tasks/multiSend";
-import "./tasks/balance"
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import { evm } from './constant';
+import './tasks/multiSend';
+import './tasks/balance';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-  defaultNetwork: "ganache0",
+  solidity: '0.8.9',
+  defaultNetwork: 'ganache0',
   networks: {
     ganache0: {
       chainId: 1337,
