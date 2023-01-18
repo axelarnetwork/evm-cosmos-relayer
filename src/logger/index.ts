674f3e302389d66f3ffa189f605b568f422e4450
npty <nptytn2@gmail.com>
2023-01-18 17:59:36 +0700

    chore: fix duplicated logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -62,7 +62,7 @@ if (process.env.NODE_ENV !== 'production') {
       })
     );
 
-    logger.add(
+    apiLogger.add(
       new transports.Http({
         ...httpTransportOptions,
         format: apiFormat,
