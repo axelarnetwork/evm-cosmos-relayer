ad628588b138d6b9f40926e0ca223f52e5ba530a
npty <nptytn2@gmail.com>
2023-03-01 00:52:57 +0700

    chore: import pk from secret.json

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,9 +1,10 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-etherscan';
-import { evm } from './constant';
 import './tasks/multiSend';
 import './tasks/balance';
+import { evm } from './constant';
+import { privateKey } from './secret.json';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
@@ -13,7 +14,7 @@ const config: HardhatUserConfig = {
       chainId: 1337,
       gasMultiplier: 2,
       url: evm.rpcUrl,
-      accounts: [evm.privateKey],
+      accounts: [privateKey],
     },
   },
 };
