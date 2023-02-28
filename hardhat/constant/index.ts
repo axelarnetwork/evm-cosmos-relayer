e8f67b9085592ed6152aac996316ec13e81370a6
npty <nptytn2@gmail.com>
2023-03-01 00:44:44 +0700

    chore: remove hardhat pk

--- hardhat/constant/index.ts
+++ hardhat/constant/index.ts
@@ -1,12 +1,10 @@
 export const evm = {
-  name: "goerli",
-  privateKey:
-    "8d5dbff28bef9e96c9324efdff1adaf6fc6c73eaed24d1f9c5038edfad590759",
+  name: 'goerli',
   rpcUrl:
-    "http://a087b4719fc8944a0952490cf1020812-853925870.us-east-2.elb.amazonaws.com:7545",
-  gateway: "0xE720c5C38028Ca08DA47E179162Eca2DD255B6eC",
+    'http://a087b4719fc8944a0952490cf1020812-853925870.us-east-2.elb.amazonaws.com:7545',
+  gateway: '0xE720c5C38028Ca08DA47E179162Eca2DD255B6eC',
 };
 
 export const cosmos = {
-  name: "demo-chain",
+  name: 'demo-chain',
 };
