c7f9cb1541da7a6a097bf376d452b0afb46c1558
npty <nptytn2@gmail.com>
2023-03-10 14:37:54 +0700

    chore: add parse ContractCallWithTokenSubmitted

--- src/utils/parseUtils.ts
+++ src/utils/parseUtils.ts
@@ -1,4 +1,4 @@
-import { IBCEvent } from '../types';
+import { ContractCallWithTokenSubmitted, IBCEvent } from '../types';
 import {
   ContractCallEventObject,
   ContractCallWithTokenEventObject,
@@ -40,6 +40,32 @@ export const parseContractCallSubmittedEvent = (
     sourceChain: removeQuote(event[`${key}.source_chain`][0]),
     destinationChain: removeQuote(event[`${key}.destination_chain`][0]),
     contractAddress: removeQuote(event[`${key}.contract_address`][0]),
+    payload: `0x${decodeBase64(removeQuote(event[`${key}.payload`][0]))}`,
+    payloadHash: `0x${decodeBase64(
+      removeQuote(event[`${key}.payload_hash`][0])
+    )}`,
+  };
+
+  return {
+    hash: event['tx.hash'][0],
+    srcChannel: event['write_acknowledgement.packet_src_channel'][0],
+    destChannel: event['write_acknowledgement.packet_dst_channel'][0],
+    args: data,
+  };
+};
+
+export const parseContractCallWithTokenSubmittedEvent = (
+  event: any
+): IBCEvent<ContractCallWithTokenSubmitted> => {
+  const key = 'axelar.axelarnet.v1beta1.ContractCallSubmitted';
+  const data = {
+    messageId: removeQuote(event[`${key}.message_id`][0]),
+    sender: removeQuote(event[`${key}.sender`][0]),
+    sourceChain: removeQuote(event[`${key}.source_chain`][0]),
+    destinationChain: removeQuote(event[`${key}.destination_chain`][0]),
+    contractAddress: removeQuote(event[`${key}.contract_address`][0]),
+    // amount: parseInt(removeQuote(event[`${key}.coins`][0])), // coin.amount and coin.denom?
+    // symbol: removeQuote(event[`${key}.symbol`][0]),
     payload: `0x${decodeBase64(removeQuote(event[`${key}.payload`][0]))}`,
     payloadHash: `0x${decodeBase64(
       removeQuote(event[`${key}.payload_hash`][0])
