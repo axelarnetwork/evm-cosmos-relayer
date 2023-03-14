940761c78aa9047525757118a092bb984dec7f2a
npty <nptytn2@gmail.com>
2023-03-14 23:24:45 +0700

    chore: update hardhat test script from ganache0 to osmo-test-4

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -4,17 +4,17 @@ import '@nomiclabs/hardhat-etherscan';
 import './tasks/multiSend';
 import './tasks/balance';
 import './tasks/osmosisTest';
-import { evm } from './constant';
+import { evm, ganache } from './constant';
 import { privateKey } from './secret.json';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
-  defaultNetwork: 'goerli',
+  defaultNetwork: 'ganache0',
   networks: {
     ganache0: {
       chainId: 1337,
       gasMultiplier: 2,
-      url: evm.rpcUrl,
+      url: ganache.rpcUrl,
       accounts: [privateKey],
     },
     goerli: {
