78a0c5b3b42ea0a84461f8f5be9e5e442c7fbed0
npty <nptytn2@gmail.com>
2023-03-30 21:05:04 +0700

    chore: fix error not properly log

--- src/handler/errorHandler.ts
+++ src/handler/errorHandler.ts
@@ -5,9 +5,8 @@ import { logger } from '../logger';
 export async function handleAnyError(db: DatabaseClient, tag: string, e: any) {
   // handle prisma error
   if (e instanceof PrismaClientInitializationError) {
-    logger.error(`[${tag}]: ${e.message}`);
-
     // disconnect the db, so the prisma client can be re-used when the db is back up
     await db.disconnect();
   }
+  logger.error(`[${tag}]: ${e.message}`);
 }
