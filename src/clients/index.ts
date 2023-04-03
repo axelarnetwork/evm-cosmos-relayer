3959d399c0d0d3a97521adf4e82629c6008e75cd
npty <nptytn2@gmail.com>
2023-04-03 11:41:26 +0700

    chore: fix clients export

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,6 +1,4 @@
 export * from './AxelarClient';
-export * from '../listeners/GMPListener';
 export * from './SigningClient';
-export * from '../../unused/IbcClient';
 export * from './EvmClient';
 export * from './DatabaseClient';
