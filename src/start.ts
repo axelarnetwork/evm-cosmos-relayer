ed9960294deefe565119718c025c32d5d513cd42
npty <nptytn2@gmail.com>
2023-04-06 18:14:21 +0700

    chore: fix imports

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,6 @@
-import { logger } from "logger";
-import { startAPIServer } from "api";
-import { startRelayer } from "relayer";
+import { logger } from "./logger";
+import { startAPIServer } from "./api";
+import { startRelayer } from "./relayer";
 
 logger.info('Starting relayer api server...');
 startAPIServer();
