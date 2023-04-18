4363534bbf52f0e4641f7cb39fef9e54c95d81b3
npty <78221556+npty@users.noreply.github.com>
2023-04-18 09:24:02 +0700

    chore: remove filter for cosmos chains (#39)

--- src/listeners/EvmListener/EvmListener.ts
+++ src/listeners/EvmListener/EvmListener.ts
@@ -6,6 +6,7 @@ import { TypedEvent } from '../../types/contracts/common';
 import { EvmEvent } from '../../types';
 import { Subject } from 'rxjs';
 import { logger } from '../../logger';
+import { env } from 'config';
 
 export class EvmListener {
   private gatewayContract: IAxelarGateway;
@@ -33,7 +34,7 @@ export class EvmListener {
       const ev: Event = args[args.length - 1];
 
       if (ev.blockNumber <= this.currentBlock) return;
-      if (!event.isAcceptedChain(this.cosmosChainNames, ev.args)) return;
+      if (env.CHAIN_ENV === 'testnet' && !event.isAcceptedChain(this.cosmosChainNames, ev.args)) return;
 
       const evmEvent = await event.parseEvent(
         this.chainId,
--- src/relayer/rxOperators.ts
+++ src/relayer/rxOperators.ts
@@ -1,5 +1,4 @@
 import { filter, of, throwError } from 'rxjs';
-import { TypedEvent } from '../types/contracts/common';
 import { EvmEvent } from '../types';
 import {
   ContractCallApprovedEventObject,
@@ -9,37 +8,30 @@ import {
 } from '../types/contracts/IAxelarGateway';
 import { CosmosNetworkConfig } from '../config/types';
 import { EvmClient } from '../clients';
+import { env } from 'config';
 
 export function filterCosmosDestination(cosmosChains: CosmosNetworkConfig[]) {
-  return filter(
-    (
-      event: EvmEvent<
-        ContractCallWithTokenEventObject | ContractCallEventObject
-      >
-    ) =>
-      cosmosChains
-        .map((chain) => chain.chainId)
-        .includes(event.args.destinationChain)
+  if (env.CHAIN_ENV === 'devnet')
+    return filter<EvmEvent<ContractCallWithTokenEventObject | ContractCallEventObject>>(() => true);
+
+  return filter((event: EvmEvent<ContractCallWithTokenEventObject | ContractCallEventObject>) =>
+    cosmosChains.map((chain) => chain.chainId).includes(event.args.destinationChain)
   );
 }
 
 export function mapEventToEvmClient(
-  event: EvmEvent<
-    ContractCallApprovedEventObject | ContractCallApprovedWithMintEventObject
-  >,
+  event: EvmEvent<ContractCallApprovedEventObject | ContractCallApprovedWithMintEventObject>,
   evmClients: EvmClient[]
 ) {
   // Find the evm client associated with event's destination chain
   const evmClient = evmClients.find(
-    (client) =>
-      client.chainId.toLowerCase() === event.destinationChain.toLowerCase()
+    (client) => client.chainId.toLowerCase() === event.destinationChain.toLowerCase()
   );
 
   // If no evm client found, return
   if (!evmClient)
     return throwError(
-      () =>
-        `No evm client found for event's destination chain ${event.destinationChain}`
+      () => `No evm client found for event's destination chain ${event.destinationChain}`
     );
 
   return of({
