6364588b97739c8f85d151988f1453d7c0a28d79
npty <78221556+npty@users.noreply.github.com>
2023-06-27 14:01:05 +0700

    chore: replace infura rpc with public rpc (#49)

--- hardhat/constant/index.ts
+++ hardhat/constant/index.ts
@@ -1,6 +1,6 @@
 export const evm = {
   name: 'goerli',
-  rpcUrl: 'https://goerli.infura.io/v3/10de1265f1234c93acfec19ca8f4afd7',
+  rpcUrl: 'https://goerli.blockpi.network/v1/rpc/public',
   gateway: '0xe432150cce91c13a887f7D836923d5597adD8E31',
   gasService: '0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6',
 };
@@ -14,15 +14,12 @@ export const avalanche = {
 
 export const ganache = {
   name: 'ganache',
-  rpcUrl:
-    'http://a5c6803b39cd942c3b9daee2847e24fa-1977123350.us-east-2.elb.amazonaws.com:7545',
+  rpcUrl: 'http://a5c6803b39cd942c3b9daee2847e24fa-1977123350.us-east-2.elb.amazonaws.com:7545',
   gateway: '0xE720c5C38028Ca08DA47E179162Eca2DD255B6eC',
 };
 
 export const osmosis = {
   name: 'osmosis-5',
-  callContract:
-    'osmo1uhd7md703jprhqprvz96fdpttlgcaftmtf8gy65ny78jkrmxr3jqtdlekz',
-  callContractWithToken:
-    'osmo1956rjtkm4rh3ncsh5cx0u8552sx80z9ppwmnmucccz3mhs0pglus4rgm8u',
+  callContract: 'osmo1uhd7md703jprhqprvz96fdpttlgcaftmtf8gy65ny78jkrmxr3jqtdlekz',
+  callContractWithToken: 'osmo1956rjtkm4rh3ncsh5cx0u8552sx80z9ppwmnmucccz3mhs0pglus4rgm8u',
 };
